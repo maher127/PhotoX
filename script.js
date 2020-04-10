b1add6ab0da638feb97a7440e7da60749187b9f9
maher127 <maher_hedhili@hotmai.fr>
2020-04-10 13:23:23 +0200

    mission-section

--- script.js
+++ script.js
@@ -14,4 +14,14 @@ $(document).ready(function () {
     $(".nav-link").removeClass("nav-active");
     $(this).addClass("nav-active");
   });
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 650) {
+      $(".camera-img").addClass("fromLeft");
+      $(".mission-text").addClass("fromRight");
+    } else {
+      $(".camera-img").removeClass("fromLeft");
+      $(".mission-text").removeClass("fromRight");
+    }
+  });
 });
